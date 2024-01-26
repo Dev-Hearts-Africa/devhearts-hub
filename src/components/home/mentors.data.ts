7de364d26eda7d5a17f1c174ecb11b48b5712d5e
Katusiime Joel Ian <joelkatsme@gmail.com>
2024-01-27 01:02:52 +0300

    New Updates

--- src/components/home/mentors.data.ts
+++ src/components/home/mentors.data.ts
@@ -43,17 +43,17 @@ export const data: Array<Mentor> = [
       logo: '/images/companies/airbnb.png',
     },
   },
-  // {
-  //   id: 4,
-  //   photo: '/images/mentors/philip-martin-5aGUyCW_PJw-unsplash.jpg',
-  //   name: 'Ethan Arinaitwe',
-  //   linkedin: "https://www.linkedin.com/in/joelkatsme/",
-  //   category: 'Fullstack Development',
-  //   description:
-  //     'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  //   company: {
-  //     name: 'Microsoft',
-  //     logo: '/images/companies/microsoft.png',
-  //   },
-  // },
+  {
+    id: 4,
+    photo: '/images/mentors/philip-martin-5aGUyCW_PJw-unsplash.jpg',
+    name: 'Ethan Arinaitwe',
+    linkedin: "https://www.linkedin.com/in/joelkatsme/",
+    category: 'Fullstack Development',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    company: {
+      name: 'Microsoft',
+      logo: '/images/companies/microsoft.png',
+    },
+  },
 ]
